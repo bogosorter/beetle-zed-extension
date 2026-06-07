1c3c19c3559afd19ef598ba6f1d67da02cbbcaae
bogosorter <luiswbarbosa@gmail.com>
2026-06-07 20:36:08 +0100

    initial commit

---
+++ languages/beetle/highlights.scm
@@ -0,0 +1,15 @@
+[
+    "if"
+    "return"
+] @keyword
+
+[
+    "integer"
+    "boolean"
+] @type
+
+(integer) @number
+(boolean) @boolean
+(symbol) @variable
+(assignment
+    functionName: (symbol) @function)
